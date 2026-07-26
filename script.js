2a33e3e9767bdb424542c2e7f6a7713372b3317e
Salieu Gbla <153820868+30Gbla@users.noreply.github.com>
2026-07-26 08:55:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+// Full Screen Gallery
+
+function openImage(src){
+
+document.getElementById("lightbox").style.display="flex";
+
+document.getElementById("lightbox-img").src=src;
+
+}
+
+function closeImage(){
+
+document.getElementById("lightbox").style.display="none";
+
+}
